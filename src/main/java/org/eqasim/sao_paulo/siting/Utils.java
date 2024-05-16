f9080cae3f816d1372eda2eae9eb2d73f16be403
Hao Wu <wu.hao@tum.de>
2024-05-16 18:39:01 +0200

    Bugfix & refactorings & new features

--- src/main/java/org/eqasim/sao_paulo/siting/Utils.java
+++ src/main/java/org/eqasim/sao_paulo/siting/Utils.java
@@ -6,17 +6,22 @@ import java.util.*;
 
 import java.util.Map;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import net.bhl.matsim.uam.infrastructure.UAMStation;
 import net.bhl.matsim.uam.infrastructure.readers.UAMXMLReader;
 import net.bhl.matsim.uam.infrastructure.UAMVehicle;
 import org.matsim.contrib.dvrp.fleet.DvrpVehicle;
+import org.matsim.contrib.dvrp.fleet.FleetSpecificationImpl;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.network.io.MatsimNetworkReader;
 
 public class Utils {
-    private static final double SEARCH_RADIUS = 1000; // Radius in meters for nearby station search
+    private static final double SEARCH_RADIUS = 400; // Radius in meters for nearby station search
     private static final int UAM_CAPACITY = 4; // Maximum number of seats in a UAM vehicle
     private static final double TELEPORTATION_SPEED = 20/3.6; // Walking speed in meters per second
 
@@ -27,7 +32,7 @@ public class Utils {
         dataLoader.loadAllData();
 
         // minutes converted to seconds
-        double POOLING_TIME_WINDOW = 10 * 60;
+        double POOLING_TIME_WINDOW = 5 * 60;
         // Define the paths to your data files
         String pooledTripsPath = "src/main/java/org/eqasim/sao_paulo/siting/output_pooled_trips.csv";
 
@@ -112,25 +117,35 @@ public class Utils {
     }
 
     private static List<List<UAMTrip>> poolTrips(List<UAMTrip> trips, double timeWindow) {
-        Map<String, List<UAMTrip>> potentialGroups = new HashMap<>();
-        for (UAMTrip trip : trips) {
-            for (UAMTrip otherTrip : trips) {
+        Map<String, Set<UAMTrip>> potentialGroups = new HashMap<>();
+        for (UAMTrip trip : trips) { // pooled trip candidate
+            for (UAMTrip otherTrip : trips) { // base trip
+                double accessTimeToStationForBaseTrip = otherTrip.calculateTeleportationTime(otherTrip.origStation);
+                double accessTimeToOldStationForPoolTrip = trip.calculateTeleportationTime(trip.origStation);
+                double accessTimeToNewStationForPoolTrip = trip.calculateTeleportationTime(otherTrip.origStation);
+                double arrivalTimeDifference = (otherTrip.departureTime+accessTimeToStationForBaseTrip - (trip.departureTime+accessTimeToNewStationForPoolTrip));
                 if (trip != otherTrip && areStationsNearby(trip.origStation, otherTrip.origStation) &&
-                        areStationsNearby(trip.destStation, otherTrip.destStation) &&
-                        Math.abs(trip.departureTime - otherTrip.departureTime) <= timeWindow) {
-                    trip.calculateTeleportationTime(otherTrip.origStation); // Calculate walking time to station
+                    areStationsNearby(trip.destStation, otherTrip.destStation) &&
+                    arrivalTimeDifference <= timeWindow && arrivalTimeDifference >= 0) {
+                    String key = otherTrip.origStation + "_" + otherTrip.destStation;
+                    potentialGroups.computeIfAbsent(key, k -> new HashSet<>()).add(otherTrip);
+                    potentialGroups.computeIfAbsent(key, k -> new HashSet<>()).add(trip);
 
-                    String key = trip.origStation + "_" + trip.destStation;
-                    potentialGroups.computeIfAbsent(key, k -> new ArrayList<>()).add(trip);
                     // Ensure that each trip is only added once per eligible grouping
                     break; // TODO: need to pool the trips optimally instead of this!
                 }
             }
         }
 
+        // Convert Set to List
+        List<List<UAMTrip>> listOfListsOfTrips = potentialGroups.values()
+                .stream()
+                .map(ArrayList::new) // Converts each Set<UAMTrip> to a List<UAMTrip>
+                .collect(Collectors.toList());
+
         // Limit each group to the capacity of the UAM vehicle
         List<List<UAMTrip>> finalGroups = new ArrayList<>();
-        for (List<UAMTrip> group : potentialGroups.values()) {
+        for (List<UAMTrip> group : listOfListsOfTrips) {
             if (group.size() > UAM_CAPACITY) {
                 group.sort(Comparator.comparingDouble(t -> t.walkingTimeToPooledStation)); // Prioritize by earliest walking time
                 finalGroups.add(new ArrayList<>(group.subList(0, UAM_CAPACITY)));
@@ -152,12 +167,26 @@ public class Utils {
         double totalIncreasedWaitTime = 0.0;
 
         for (List<UAMTrip> group : pooledGroups) {
-            UAMTrip baseTrip = group.get(0);
+            //identify the base trip which has the earliest departure time
+            UAMTrip baseTrip = group.stream().max(Comparator.comparing(UAMTrip::getDepartureTime)).orElse(null);
+/*            UAMTrip baseTrip = null;
+            if (!group.isEmpty()) {
+                baseTrip = group.get(0);  // Start with the first trip as the base
+                for (UAMTrip pooledTrip : group) {
+                    if (pooledTrip.getDepartureTime().compareTo(baseTrip.getDepartureTime()) < 0) {
+                        baseTrip = pooledTrip;  // Found a trip with an earlier departure time
+                    }
+                }
+            }*/
+
             for (UAMTrip pooledTrip : group) {
                 if (!pooledTrip.equals(baseTrip)) {
                     savedDistance += baseTrip.flightDistance;
                     pooledTrips++;
-                    totalIncreasedWaitTime += (pooledTrip.departureTime - baseTrip.departureTime);
+                    double accessTimeToStationForBaseTrip = baseTrip.calculateTeleportationTime(baseTrip.origStation);
+                    double accessTimeToOldStationForPoolTrip = pooledTrip.calculateTeleportationTime(pooledTrip.origStation);
+                    double accessTimeToNewStationForPoolTrip = pooledTrip.calculateTeleportationTime(baseTrip.origStation);
+                    totalIncreasedWaitTime += (baseTrip.getDepartureTime() + accessTimeToStationForBaseTrip) - (pooledTrip.getDepartureTime() + accessTimeToOldStationForPoolTrip);
                 }
             }
         }
@@ -192,11 +221,11 @@ public class Utils {
     }
 
     public static class UAMTrip {
-        String tripId;
-        double originX, originY, destX, destY, departureTime, flightDistance;
-        UAMStation origStation, destStation; // Changed to Integer to handle null values
-        String purpose, income;
-        double walkingTimeToPooledStation; // Time to walk to the station
+        private String tripId;
+        private double originX, originY, destX, destY, departureTime, flightDistance;
+        private UAMStation origStation, destStation; // Changed to Integer to handle null values
+        private String purpose, income;
+        private double walkingTimeToPooledStation; // Time to walk to the station
 
         UAMTrip(String tripId, double originX, double originY, double destX, double destY, double departureTime, double flightDistance, UAMStation origStation, UAMStation destStation, String purpose, String income) {
             this.tripId = tripId;
@@ -212,10 +241,32 @@ public class Utils {
             this.income = income;
         }
 
+        // TODO: Use MATSim to calculate the routes and travel distances
+        public double calculateTeleportationDistance(UAMStation station) {
+            return Math.sqrt(Math.pow(originX - station.getLocationLink().getCoord().getX(), 2) + Math.pow(originY - station.getLocationLink().getCoord().getY(), 2));
+        }
         // TODO: Use MATSim to calculate the routes and travel times
-        void calculateTeleportationTime(UAMStation station) {
-            double distance = Math.sqrt(Math.pow(originX - station.getLocationLink().getCoord().getX(), 2) + Math.pow(originY - station.getLocationLink().getCoord().getY(), 2));
+        public double calculateTeleportationTime(UAMStation station) {
+            double distance = calculateTeleportationDistance(station);
             walkingTimeToPooledStation = distance / TELEPORTATION_SPEED;
+            return walkingTimeToPooledStation;
+        }
+
+        // getDepartureTime
+        public double getDepartureTime() {
+            return departureTime;
+        }
+        //getOriginX
+        public double getOriginX() {
+            return originX;
+        }
+        //getOriginY
+        public double getOriginY() {
+            return originY;
+        }
+        //getOriginStation
+        public UAMStation getOriginStation() {
+            return origStation;
         }
     }
 
@@ -257,6 +308,12 @@ public class Utils {
 
         public List<UAMTrip> getUamTrips() {
             return uamTrips;
+        }
+        public Map<Id<DvrpVehicle>, UAMVehicle> getVehicles() {
+            return vehicles;
+        }
+        public Map<Id<UAMStation>, UAMStation> getStations() {
+            return stations;
         }
     }
 
